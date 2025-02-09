d236a3f571e29fe9fe5edcb8473f13e16daae9fb
Dina425 <bazhenovadina@gmail.com>
2025-02-09 08:42:57 +0200

    Hw

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,35 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HelperUser extends HelperBase {
     public HelperUser(WebDriver wd) {
         super(wd);
     }
+    public void openLoginRegistrationForm(){
+
+        click(By.xpath("/*[@class='navigation-link' and contains(@href,'/login')]"));
+    }
+    public void fillLoginRegistrationform(String email, String password) {
+
+        type(By.xpath("//input[@id='email']"),email);
+        type(By.xpath("//input[@id='password']"),password);
+
+    }
+
+    public void submitLogin(){
+        click(By.xpath("//button[contains(text(),'Y’alla!')]"));
+    }
+    public void submitOkButton(){
+        click(By.xpath("//button[normalize-space()='Ok']"));
+    }
+    public boolean isLogged() {
+        return isElementPresent(By.xpath("//a[normalize-space()='Logout']"));
+    }
+    public void logout() {
+        click(By.xpath("//a[normalize-space()='Logout']"));
+    }
+
+
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,10 +1,23 @@
 package tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase{
+    @BeforeMethod
+    public void preCondition(){
+        // if SignOut present---logout
+        if(app.getHelperUser().isLogged())
+            app.getHelperUser().logout();
+    }
+
     @Test
-    public void test(){
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        //app.getHelperUser().fillLoginRegistrationform("bazhenovadina321@gmail.com","@12345Ab");
+        //app.getHelperUser().submitLogin();
+       // app.getHelperUser().submitOkButton();
+
 
     }
 }
